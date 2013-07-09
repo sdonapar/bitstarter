93d33efb0965505ea015892c61286973e5480ec6
Sasidhar Donaparthi <sasidhar.donaparthi@gmail.com>
2013-07-09 17:20:38 +0000

    corrcted syntax changes

--- web.js
+++ web.js
@@ -8,8 +8,8 @@ app.get('/', function(request, response) {
   {
    if (err) throw err;
    response.send(data.toString());
+  });
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
